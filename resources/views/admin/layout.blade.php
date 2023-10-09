eb2e7f772aa74546bc381991cf909e3c84930164
Nguyễn Huy Thiện <nt.thien2003abc@gmail.com>
2023-10-09 20:16:06 +0700

    first commit

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>BeeSmart - Admin</title>
+    <title>BeeSmart - Admin no</title>
 
     <!-- Custom fonts for this template-->
     <link href="/ad/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
