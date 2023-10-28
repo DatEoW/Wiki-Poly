9a35d88c2f4196c40a030fd4c24dbca7ad83d4bb
HuuNguyen15 <huunt1592@gmail.com>
2023-10-28 21:37:57 +0700

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravwwdsdsdsdsdwel</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -30,6 +30,7 @@
                     @endauth
                 </div>
             @endif
+
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
